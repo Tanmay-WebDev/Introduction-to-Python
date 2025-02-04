f973ae8b43f7d33f5f74954ed5860e42c27bd6a1
Tanmay-WebDev <umas81261@gmail.com>
2025-02-04 19:00:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# Use Of Print Statement
+print("Hey, My name is Tanmay Singh and now i am learning about Python.")
+# Stored as String
+print("12")
+# Stored as a number
+print(12)
+# For Break Line
+print("\n")
+# Example of Line Break
+print("Line Break")
+# Importing of Keyword for list
+import keyword
+# Print of Keywords
+print(keyword.kwlist)
